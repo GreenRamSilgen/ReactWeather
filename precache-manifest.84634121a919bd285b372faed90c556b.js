72f8d741b33625290bf8f4667775521c7fe13109
GreenRamSilgen <ps3iswow@gmail.com>
2020-08-17 13:26:55 -0700

    Updates

---
+++ precache-manifest.84634121a919bd285b372faed90c556b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9775e4ae475793a6e310821e8a02017",
+    "url": "/ReactWeather/index.html"
+  },
+  {
+    "revision": "127221977a16a2f2701c",
+    "url": "/ReactWeather/static/css/main.98ec3b52.chunk.css"
+  },
+  {
+    "revision": "e8fbb6935bd6c08d0dac",
+    "url": "/ReactWeather/static/js/2.5be7f423.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactWeather/static/js/2.5be7f423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "127221977a16a2f2701c",
+    "url": "/ReactWeather/static/js/main.d93085c5.chunk.js"
+  },
+  {
+    "revision": "b211f174984dec8df306",
+    "url": "/ReactWeather/static/js/runtime-main.ca5c79ca.js"
+  }
+]);
