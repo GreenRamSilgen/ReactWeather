f0d4807dc3560d11d0debbc564cbc4825df87045
GreenRamSilgen <ps3iswow@gmail.com>
2020-08-17 13:33:09 -0700

    securing fetch request

--- src/javascript/ApiManager.js
+++ src/javascript/ApiManager.js
@@ -9,7 +9,7 @@ let ApiManager = (function(){
          */
         getWeatherCS:  async function cityAndStateApiCall({city,state,unit="imperial"}){
             let paragraph = '';
-            let htmlResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=75685a105e4030cddbdf93dfd89f6fe9&units=${unit}`)
+            let htmlResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=75685a105e4030cddbdf93dfd89f6fe9&units=${unit}`)
             let reader = await htmlResponse.body.getReader();
             let readUint8Array = await reader.read();
             for(let val of readUint8Array.value){
@@ -23,7 +23,7 @@ let ApiManager = (function(){
         
         getWeatherCoord:  async function longAndLatApiCall({lat,lon,unit="imperial"}){
             let paragraph = '';
-            let htmlResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=75685a105e4030cddbdf93dfd89f6fe9&units=${unit}`)
+            let htmlResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=75685a105e4030cddbdf93dfd89f6fe9&units=${unit}`)
             let reader = await htmlResponse.body.getReader();
             let readUint8Array = await reader.read();
             for(let val of readUint8Array.value){
